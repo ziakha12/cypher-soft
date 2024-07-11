9e93aa591ba0bfa5a825c2292df073cfc93d1083
ziakha12 <kzzia06@gmail.com>
2024-07-10 18:48:59 -0700

    finalize project

---
+++ js/blog/category-data.js
@@ -0,0 +1,200 @@
+const categoryData = [
+  {
+    "category": "Markets",
+    "totalPost": 4,
+    "posts": [
+      {
+        "title": "BlackRock responds to George Soros’ criticism over China investments",
+        "link": "blackrock-responds-to-george-soros-criticism-over.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": "img/user/image-post-1js43jtw649.jpg",
+        "date": "October 19, 2021",
+        "content": "LONDON - BlackRock, the world’s largest asset manager, has responded to sharp criticism from billionaire investor George Soros over the firm’s investments in China.&nbsp; "
+      },
+      {
+        "title": "Gold holds near 2-1/2-month high on prospects of Fed taper delay",
+        "link": "gold-holds-near-2-12-month-high-on-prospects-of-fed.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "October 19, 2021",
+        "content": "Gold prices hovered on Monday below a 2-1/2-month peak after a disappointing U.S. jobs data signaled that the Federal Reserve could push back the timeline for tapering stimulus measures, bolstering bullion’s appeal as an inflation hedge.&nbsp;&nbsp;"
+      },
+      {
+        "title": "The six reasons why Jim Cramer is concerned about the stock market in September",
+        "link": "the-six-reasons-why-jim-cramer-is-concerned-about-the.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "September 09, 2021",
+        "content": "Jim Cramer said Wednesday he sees a number of worrisome factors that are likely to contribute to market volatility in September, beyond just the fact it is a historically&nbsp;tough month for stocks.&nbsp; "
+      },
+      {
+        "title": "European markets head for lower open ahead of European Central Bank meeting",
+        "link": "european-markets-head-for-lower-open-ahead-of-european.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "September 09, 2021",
+        "content": "LONDON — European stocks are expected to open lower on Thursday as investors in the region watch for announcements from the European Central Bank."
+      }
+    ]
+  },
+  {
+    "category": "Health",
+    "totalPost": 2,
+    "posts": [
+      {
+        "title": "India is ‘better equipped’ to deal with a potential Covid third wave, WHO chief scientist says",
+        "link": "india-is-better-equipped-to-deal-with-a-potential.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "October 19, 2021",
+        "content": "India is better prepared to stop new Covid-19 waves from wreaking havoc on the country, according to Dr. Soumya Swaminathan, chief scientist at the World Health Organization."
+      },
+      {
+        "title": "WHO says delta remains the ‘most concerning’ Covid variant despite emergence of mu",
+        "link": "who-says-delta-remains-the-most-concerning-covid.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "September 09, 2021",
+        "content": "The fast-spreading delta variant remains the “most concerning” coronavirus strain despite the emergence of the mu variant, World Health Organization officials said Tuesday."
+      }
+    ]
+  },
+  {
+    "category": "Politics",
+    "totalPost": 2,
+    "posts": [
+      {
+        "title": "China portrays the U.S. as ‘weak and unreliable’ to draw Taiwan closer, analyst says",
+        "link": "china-portrays-the-us-as-weak-and-unreliable-to.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "October 19, 2021",
+        "content": "In an attempt to draw Taiwan closer, China is trying to portray the U.S. as a weak power that cannot be counted on — by highlighting the Taliban’s swift takeover of Afghanistan, a geopolitical expert told CNBC&nbsp;&nbsp;"
+      },
+      {
+        "title": "Kamala Harris says Asia won’t have to choose between the U.S. and China",
+        "link": "kamala-harris-says-asia-wont-have-to-choose-between.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "September 09, 2021",
+        "content": "SINGAPORE - Vice President Kamala Harris on Tuesday called out Beijing’s actions in the South China Sea, and sought to assure countries in Asia that they won’t have to choose between the U.S. and China.&nbsp;&nbsp;"
+      }
+    ]
+  },
+  {
+    "category": "Crypto",
+    "totalPost": 3,
+    "posts": [
+      {
+        "title": "Ukraine is the latest country to legalize bitcoin as the cryptocurrency slowly goes global",
+        "link": "ukraine-is-the-latest-country-to-legalize-bitcoin-as.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": "img/user/image-post-01v41bpa71t7.jpg",
+        "date": "October 19, 2021",
+        "content": "Ukraine is the fifth country in as many weeks to lay down some ground rules for the cryptocurrency market, a sign that governments around the world are realizing that&nbsp;bitcoin&nbsp;is here to stay.&nbsp;&nbsp;"
+      },
+      {
+        "title": "Bitcoin ‘whales’ jump back into market during cryptocurrency’s rebound to $50,000",
+        "link": "bitcoin-whales-jump-back-into-market-during-cryptocurrencys.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "October 19, 2021",
+        "content": "Bitcoin’s biggest investors appear to be upping their positions as the price recovers. Cryptocurrency accounts holding roughly $50 million or more worth of&nbsp;bitcoin&nbsp;have steadily increased their buying since the end of June, according to new data from blockchain firm Chainalysis. "
+      },
+      {
+        "title": "Coinbase shares fall after it reveals SEC plans to sue over interest-earning product",
+        "link": "coinbase-shares-fall-after-it-reveals-sec-plans-to.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "September 09, 2021",
+        "content": "Coinbase has received notice of a possible enforcement action from the Securities and Exchange Commission related to its interest-earning product, which the company had planned to launch in the coming weeks."
+      }
+    ]
+  },
+  {
+    "category": "Technology",
+    "totalPost": 3,
+    "posts": [
+      {
+        "title": "Biden Administration outlines how solar could be nearly half of electricity supply by 2050",
+        "link": "biden-administration-outlines-how-solar-could-be-nearly.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": "img/user/image-post-gzn1tktw0mm.jpg",
+        "date": "October 19, 2021",
+        "content": "The White House on Wednesday outlined a plan for solar energy to supply nearly half of the nation’s electricity by 2050. The ambitious outline would see solar rising from 3% of generation in 2020 to 40% by 2035 before ultimately hitting 45% by 2050.&nbsp; "
+      },
+      {
+        "title": "Tech giants are rushing to develop their own chips — here’s why",
+        "link": "tech-giants-are-rushing-to-develop-their-own-chips.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "September 09, 2021",
+        "content": "Not content with relying on standard chips that are in high demand, some of the world’s biggest tech firms are developing their own semiconductors."
+      },
+      {
+        "title": "Wind turbine giant Siemens Gamesa claims world-first in blade recycling",
+        "link": "wind-turbine-giant-siemens-gamesa-claims-world-first.html",
+        "author": {
+          "id": "bk1baa",
+          "name": "Adiba Bachtiar",
+          "avatar": "img/user/author-lybqnhew0wc.jpg"
+        },
+        "image": false,
+        "date": "September 09, 2021",
+        "content": "Siemens Gamesa Renewable Energy said Tuesday it had launched a recyclable wind turbine blade, a move which represents the latest example of how the industry is attempting to find ways to re-use materials."
+      }
+    ]
+  }
+]
